66cfedd7555018675214e81db016a36d86acddde
Bhavesh <bhaveshsonkar952@gmail.com>
2026-08-14 00:28:53 +0530

    feat: add API_URL and APP_BUILD_TAG configuration constants

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://script.google.com/macros/s/AKfycby4RNwxBEfKWLWCT4Y6-LFLkObAE-j4LCDBUh5Lc3eG6zAcPN1WvUqXwOXMyWDH3nA/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbzqtSn6Xm9DSOUJTTcC_mdkvSLkUBIoNdhfr-oE2ET6WyPYQys9FWgPdecsu4sbXXA/exec";
 export const APP_BUILD_TAG = "script-20260410-guard-logs-1";
