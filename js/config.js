17fbd9800e7021b001e80d0d36d77fa05fdf5d47
Bhavesh <bhaveshsonkar952@gmail.com>
2026-08-20 02:23:05 +0530

    feat: initialize build assets and configuration files

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://script.google.com/macros/s/AKfycbxlhguDsnDpNKmOqBh6jkY_ZuTDspIMBu4AbTA_jSALcCv4vqQSD3XZPN0P5gXwwM8/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbx8f53eCUzsEtNnxwh1f1l-V3KjEWC1yjQmC9LlDgaAEsX6J_I5yco3jVWTlhgfyRw/exec";
 export const APP_BUILD_TAG = "script-20260410-guard-logs-1";
